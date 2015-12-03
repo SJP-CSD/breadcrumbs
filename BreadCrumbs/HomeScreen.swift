7d6ee2218c8b708b4ddf4c65bb53ec49fe11a9aa
Crepox <jstraceski16@hs132-13.stjohnsprep.org>
2015-12-03 08:30:56 -0500

    Fixing file locations

---
+++ BreadCrumbs/HomeScreen.swift
@@ -0,0 +1,26 @@
+//
+//  HomeScreen.swift
+//  BreadCrumbs
+//
+//  Created by Joseph Straceski on 12/1/15.
+//  Copyright (c) 2015 Joseph Straceski. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class HomeScreen: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        super.selectedIndex = 1
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
