4e5b6168b8195c6cf0696a1d4c09fabfe90d835c
wparrkerr <wparker16@hs132-15.stjohnsprep.org>
2015-12-10 13:29:00 -0500

    Map Stuff
    featuring willenMAtt

---
+++ BreadCrumbs/CustomObject.m
@@ -0,0 +1,9 @@
+//
+//  CustomObject.m
+//  BreadCrumbs
+//
+//  Created by William Parker on 12/10/15.
+//  Copyright (c) 2015 Joseph Straceski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
--- BreadCrumbs/MapViewController.swift
+++ BreadCrumbs/MapViewController.swift
@@ -7,14 +7,85 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    let locationManager = CLLocationManager()
+    var didPressButton = false
+    var didInit = false
+    var follow = true
+    var drawPath = false
+    
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.requestAlwaysAuthorization()
+        mapView.delegate = self
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        else {
+            print("Location Services Disabled")
+        }
+        
+    }
+    
+    @IBAction func dropPin(sender: AnyObject) {
+        locationManager.startUpdatingLocation()
+        didPressButton = true
+    }
+    
+    
+    @IBAction func toggleFollow() {
+        follow = !follow
+    }
+    
+    @IBAction func togglePath() {
+        drawPath = !drawPath
+    }
+
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        
+        if didPressButton {
+            let anno: MKPointAnnotation = MKPointAnnotation()
+            anno.coordinate = locValue
+            anno.title = "Matt G."
+            anno.subtitle = "Also Will Parker"
+            mapView.addAnnotation(anno)
+            didPressButton = false
+        }
+        if didInit == false || follow
+        {
+            mapView.setRegion(MKCoordinateRegionMake(locValue, MKCoordinateSpanMake(0, 0)), animated: true)
+            didInit = true
+        }
+        
+        
+    }
+    /*  XCODE 7 XCPODE 7 XKPDE 7 ZKDD 7
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+    let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+    outputLabel.text = "\(locValue.latitude) \(locValue.longitude)"
+    
+    mapView.setRegion(MKCoordinateRegionMake(locValue, MKCoordinateSpanMake(0, 0)), animated: true)
+    //Find out what coordinate thing does
+    }*/
+    
+    func locationManager(manager: CLLocationManager,
+        didFailWithError error: NSError)
+    {
+        
     }
 }
+
 
