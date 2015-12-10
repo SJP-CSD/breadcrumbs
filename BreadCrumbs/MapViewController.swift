5ab5850b9177182df631b2eb9e5d94ebb6514f37
wparrkerr <wparker16@hs132-15.stjohnsprep.org>
2015-12-10 12:49:37 -0500

    Made MapViewController

--- BreadCrumbs/MapViewController.swift
+++ BreadCrumbs/MapViewController.swift
@@ -8,18 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
